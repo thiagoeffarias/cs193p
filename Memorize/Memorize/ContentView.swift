d5ad1350aaf4a78215bbb5694503607104af7fb4
thiagofarias <thiagoferreirafarias@gmail.com>
2024-03-27 17:01:27 +0000

    Lecture 1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Thiago Farias on 27/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    
+    var body: some View {
+        ZStack ( content :{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
