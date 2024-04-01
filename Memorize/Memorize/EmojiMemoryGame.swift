788d3c732d6da59e89248748f6cbdb5eb1136c72
Thiago Farias <thiagoferreirafarias@gmail.com>
2024-04-01 17:22:11 +0100

    Finishes lecture 3 activities. Creates model and Viewmodel. Adds dark model background color for the cards.

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Thiago Farias on 01/04/2024.
+//
+
+import Foundation
---
+++ Memorize/Memorize/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Thiago Farias on 01/04/2024.
+//
+
+import Foundation
